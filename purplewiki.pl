f32e0c488c6c71b436b79797f715f13e060ee89a
eekim <eekim@8e67a9af-bad3-0310-9569-fa3748d2677d>
2002-10-09 07:53:06 +0000

    New option for specifying view driver.

--- purplewiki.pl
+++ purplewiki.pl
@@ -4,7 +4,7 @@ use IO::File;
 use PurpleWiki::Tree;
 
 if (@ARGV < 1) {
-  print "Usage: $0 wikifile.txt\n";
+  print "Usage: $0 wikifile.txt [output_driver]\n";
   exit;
 }
 
@@ -13,7 +13,13 @@ my $wiki = PurpleWiki::Tree->new('title'=>$ARGV[0]);
 
 $wiki->parse($wikiContent);
 
-$wiki->view('XHTML');
+if (@ARGV == 2) {
+    $wiki->view($ARGV[1]);
+}
+else {
+    $wiki->view('Debug');
+}
+
 #$wiki->view('XHTML','collapse'=>[2]);
 #$wiki->view('Text');
 #$wiki->view('Purple');
