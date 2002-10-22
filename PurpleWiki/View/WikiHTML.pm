07e23638c5c86b2758980cee2224baa6ed472654
cdent <cdent@8e67a9af-bad3-0310-9569-fa3748d2677d>
2002-10-22 08:00:15 +0000

    WikiHTML is XHTML without the header and footer code. The Wiki needs to be able to do that itself (at the moment).

---
+++ PurpleWiki/View/WikiHTML.pm
@@ -0,0 +1,226 @@
+package PurpleWiki::View::WikiHTML;
+
+use PurpleWiki::Tree;
+
+# globals
+
+my @sectionState;
+
+my %structuralActionMap = (
+               'section' => {
+                   'pre' => sub { push @sectionState, 'section' },
+                   'mid' => \&_traverseStructuralWithChild,
+                   'post' => sub { pop @sectionState },
+               },
+               'indent' => {
+                   'pre' => sub { print "<div class=\"indent\">\n"},
+                   'mid' => \&_traverseStructuralWithChild,
+                   'post' => sub { print "</div>"},
+               },
+               'ul' => {
+                   'pre' => sub { print "<ul>\n" },
+                   'mid' => \&_traverseStructuralWithChild,
+                   'post' => sub { print "</ul>" },
+               },
+               'ol' => {
+                   'pre' => sub { print "<ol>\n" },
+                   'mid' => \&_traverseStructuralWithChild,
+                   'post' => sub { print "</ol>" },
+               },
+               'dl' => {
+                   'pre' => sub { print "<dl>\n" },
+                   'mid' => \&_traverseStructuralWithChild,
+                   'post' => sub { print "</dl>"},
+               },
+               'h' => {
+                   'pre' => sub { my $nid = shift;
+                                  print '<h' . &_headerLevel . '>';
+                                  &_printAnchor($nid); },
+                   'mid' => \&_traverseInlineIfContent,
+                   'post' => sub { my $nid = shift;
+                                   &_printNid($nid);
+                                   print '</h' . &_headerLevel . '>' },
+               },
+               'p' => {
+                   'pre' => sub { my $nid = shift;
+                                  print '<p>';
+                                  &_printAnchor($nid); },
+                   'mid' => \&_traverseInlineIfContent,
+                   'post' => sub { my $nid = shift;
+                                   &_printNid($nid);
+                                   print '</p>'; },
+               },
+               'li' => {
+                   'pre' => sub { my $nid = shift;
+                                  print '<li>';
+                                  &_printAnchor($nid); },
+                   'mid' => \&_traverseInlineIfContent,
+                   'post' => sub { my $nid = shift;
+                                   &_printNid($nid);
+                                   print '</li>'; },
+               },
+               'dd' => {
+                   'pre' => sub { my $nid = shift;
+                                  print '<dd>';
+                                  &_printAnchor($nid); },
+                   'mid' => \&_traverseInlineIfContent,
+                   'post' => sub { my $nid = shift;
+                                   &_printNid($nid);
+                                   print '</dd>'; },
+               },
+               'dt' => {
+                   'pre' => sub { my $nid = shift;
+                                  print '<dt>';
+                                  &_printAnchor($nid); },
+                   'mid' => \&_traverseInlineIfContent,
+                   'post' => sub { my $nid = shift;
+                                   &_printNid($nid);
+                                   print '</dt>'; },
+               },
+               'pre' => {
+                   'pre' => sub { my $nid = shift;
+                                  print '<pre>';
+                                  &_printAnchor($nid); },
+                   'mid' => \&_traverseInlineIfContent,
+                   'post' => sub { my $nid = shift;
+                                   &_printNid($nid);
+                                   print '</pre>'; },
+               },
+               );
+
+my %inlineActionMap = (
+             'b' => {
+                 'pre' => sub { print '<b>' },
+                 'mid' => \&_traverseInlineWithData,
+                 'post' => sub {print '</b>' },
+             },
+             'i' => {
+                 'pre' => sub { print '<i>' },
+                 'mid' => \&_traverseInlineWithData,
+                 'post' => sub { print '</i>' },
+             },
+             'tt' => {
+                 'pre' => sub { print '<tt>' },
+                 'mid' => \&_traverseInlineWithData,
+                 'post' => sub { print '</tt>' },
+             },
+             'text' => {
+                 'pre' => sub {},
+                 'mid' => \&_printInlineData,
+                 'post' => sub {}
+             },
+             'nowiki' => {
+                 'pre' => sub {},
+                 'mid' => \&_printInlineData,
+                 'post' => sub {}
+             }
+             );
+
+sub view {
+    my ($wikiTree, %params) = @_;
+
+    &_printHeader($wikiTree->title, $wikiTree->lastNid);
+    &_traverseStructural($wikiTree->root->children, 0);
+    &_printFooter;
+}
+
+sub _traverseStructural {
+    my ($nodeListRef, $indentLevel) = @_;
+
+    if ($nodeListRef) {
+        foreach my $node (@{$nodeListRef}) {
+            if (defined($structuralActionMap{$node->type})) {
+                &{$structuralActionMap{$node->type}{'pre'}}($node->id);
+                &{$structuralActionMap{$node->type}{'mid'}}($node,
+                                                            $indentLevel);
+                &{$structuralActionMap{$node->type}{'post'}}($node->id);
+            } 
+            &_terminateLine unless ($node->type eq 'section');
+        }
+    }
+}
+
+sub _terminateLine {
+    print "\n";
+}
+
+sub _traverseInlineIfContent {
+    my $structuralNode = shift;
+    my $indentLevel = shift;
+    if ($structuralNode->content) {
+        _traverseInline($structuralNode->content, $indentLevel);
+    }
+}
+
+sub _traverseInlineWithData {
+    my $inlineNode = shift;
+    my $indentLevel = shift;
+    _traverseInline($inlineNode->children, $indentLevel);
+}
+
+sub _printInlineData {
+    my $inlineNode = shift;
+    print &_quoteHtml($inlineNode->content);
+}
+
+sub _traverseStructuralWithChild {
+    my $structuralNode = shift;
+    my $indentLevel = shift;
+    _traverseStructural($structuralNode->children, $indentLevel + 1);
+}
+
+sub _traverseInline {
+    my ($nodeListRef, $indentLevel) = @_;
+
+    foreach my $inlineNode (@{$nodeListRef}) {
+        if ($inlineNode->type eq 'link') {
+            print '<a href="' . $inlineNode->href . '">';
+            print &_quoteHtml($inlineNode->content);
+            print '</a>';
+        }
+        elsif (defined($inlineActionMap{$inlineNode->type})) {
+            &{$inlineActionMap{$inlineNode->type}{'pre'}};
+            &{$inlineActionMap{$inlineNode->type}{'mid'}}($inlineNode,
+                                                          $indentLevel);
+            &{$inlineActionMap{$inlineNode->type}{'post'}};
+        }
+    }
+}
+
+sub _quoteHtml {
+    my ($html) = @_;
+
+    $html =~ s/&/&amp;/g;
+    $html =~ s/</&lt;/g;
+    $html =~ s/>/&gt;/g;
+    if (1) {   # Make an official option?
+        $html =~ s/&amp;([#a-zA-Z0-9]+);/&$1;/g;  # Allow character references
+    }
+    return $html;
+}
+
+sub _headerLevel {
+    my $headerLevel = scalar @sectionState + 1;
+    $headerLevel = 6 if ($headerLevel > 6);
+    return $headerLevel;
+}
+
+sub _printAnchor {
+    my $nid = shift;
+    print '<a name="0' . $nid . '" id="0' . $nid . '"></a>';
+}
+
+sub _printNid {
+    my $nid = shift;
+    print ' &nbsp;&nbsp; <a class="nid" href="#0' . $nid . '">';
+    print "(0$nid)</a>";
+}
+
+sub _printHeader {
+}
+
+sub _printFooter {
+}
+
+
+1;
