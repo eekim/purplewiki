8b9aa3dc617d0781fe4320e2162c584a314fa19e
eekim <eekim@8e67a9af-bad3-0310-9569-fa3748d2677d>
2002-11-22 20:51:10 +0000

    Added copyright notice; stub documentation.

--- PurpleWiki/InlineNode.pm
+++ PurpleWiki/InlineNode.pm
@@ -1,6 +1,39 @@
+# PurpleWiki::InlineNode.pm
+#
+# $Id: InlineNode.pm,v 1.5 2002/11/22 20:51:10 eekim Exp $
+#
+# Copyright (c) Blue Oxen Associates 2002.  All rights reserved.
+#
+# This file is part of PurpleWiki.  PurpleWiki is derived from:
+#
+#   UseModWiki v0.92          (c) Clifford A. Adams 2000-2001
+#   AtisWiki v0.3             (c) Markus Denker 1998
+#   CVWiki CVS-patches        (c) Peter Merel 1997
+#   The Original WikiWikiWeb  (c) Ward Cunningham
+#
+# PurpleWiki is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
 package PurpleWiki::InlineNode;
 
+use 5.006;
 use strict;
+use warnings;
+
+our $VERSION = '0.1';
 
 ### constructor
 
@@ -62,3 +95,28 @@ sub populate {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+PurpleWiki::InlineNode - Inline node object
+
+=head1 SYNOPSIS
+
+  use PurpleWiki::InlineNode;
+
+=head1 DESCRIPTION
+
+blah blah blah
+
+=head1 AUTHORS
+
+Chris Dent, E<lt>cdent@blueoxen.orgE<gt>
+
+Eugene Eric Kim, E<lt>eekim@blueoxen.orgE<gt>
+
+=head1 SEE ALSO
+
+L<PurpleWiki::StructuralNode>.
+
+=cut
