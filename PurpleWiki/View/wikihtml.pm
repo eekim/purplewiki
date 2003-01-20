bb80099501de725bfed0d2b1b6d1c774a50cfe22
eekim <eekim@8e67a9af-bad3-0310-9569-fa3748d2677d>
2003-01-20 21:06:46 +0000

    Refactored to use EventHandler.pm.

--- PurpleWiki/View/wikihtml.pm
+++ PurpleWiki/View/wikihtml.pm
@@ -1,6 +1,6 @@
 # PurpleWiki::View::wikihtml.pm
 #
-# $Id: wikihtml.pm,v 1.1 2003/01/18 05:23:45 eekim Exp $
+# $Id: wikihtml.pm,v 1.1.2.1 2003/01/20 21:06:46 eekim Exp $
 #
 # Copyright (c) Blue Oxen Associates 2002-2003.  All rights reserved.
 #
@@ -29,243 +29,198 @@
 
 package PurpleWiki::View::wikihtml;
 
+use 5.005;
+use strict;
 use PurpleWiki::Page;
 use PurpleWiki::Tree;
+use PurpleWiki::View::EventHandler;
 
 # globals
 
 my @sectionState;
 
-my %structuralActionMap = (
-               'section' => {
-                   'pre' => sub { push @sectionState, 'section'; return; },
-                   'mid' => \&_traverseStructuralWithChild,
-                   'post' => sub { pop @sectionState; return; },
-               },
-               'indent' => {
-                   'pre' => sub { return "<div class=\"indent\">\n"},
-                   'mid' => \&_traverseStructuralWithChild,
-                   'post' => sub { return "</div>"},
-               },
-               'ul' => {
-                   'pre' => sub { return "<ul>\n" },
-                   'mid' => \&_traverseStructuralWithChild,
-                   'post' => sub { return "</ul>" },
-               },
-               'ol' => {
-                   'pre' => sub { return "<ol>\n" },
-                   'mid' => \&_traverseStructuralWithChild,
-                   'post' => sub { return "</ol>" },
-               },
-               'dl' => {
-                   'pre' => sub { return "<dl>\n" },
-                   'mid' => \&_traverseStructuralWithChild,
-                   'post' => sub { return "</dl>"},
-               },
-               'h' => {
-                   'pre' => sub { my $nid = shift;
-                                  return '<h' . &_headerLevel . '>' .
-                                      &_printAnchor($nid); },
-                   'mid' => \&_traverseInlineIfContent,
-                   'post' => sub { my $nid = shift;
-                                   return &_printNid($nid) .
-                                       '</h' . &_headerLevel . '>'; }
-               },
-               'p' => {
-                   'pre' => sub { my $nid = shift;
-                                  return '<p>' .
-                                      &_printAnchor($nid); },
-                   'mid' => \&_traverseInlineIfContent,
-                   'post' => sub { my $nid = shift;
-                                   return &_printNid($nid) .
-                                       '</p>'; },
-               },
-               'li' => {
-                   'pre' => sub { my $nid = shift;
-                                  return '<li>' .
-                                      &_printAnchor($nid); },
-                   'mid' => \&_traverseInlineIfContent,
-                   'post' => sub { my $nid = shift;
-                                   return &_printNid($nid) .
-                                       '</li>'; },
-               },
-               'dd' => {
-                   'pre' => sub { my $nid = shift;
-                                  return '<dd>' .
-                                      &_printAnchor($nid); },
-                   'mid' => \&_traverseInlineIfContent,
-                   'post' => sub { my $nid = shift;
-                                   return &_printNid($nid) .
-                                       '</dd>'; },
-               },
-               'dt' => {
-                   'pre' => sub { my $nid = shift;
-                                  return '<dt>' .
-                                      &_printAnchor($nid); },
-                   'mid' => \&_traverseInlineIfContent,
-                   'post' => sub { my $nid = shift;
-                                   return &_printNid($nid) .
-                                       '</dt>'; },
-               },
-               'pre' => {
-                   'pre' => sub { my $nid = shift;
-                                  return '<pre>' .
-                                      &_printAnchor($nid); },
-                   'mid' => \&_traverseInlineIfContent,
-                   'post' => sub { my $nid = shift;
-                                   return &_printNid($nid) .
-                                       '</pre>'; },
-               },
-               );
+# structural node event handlers
 
-my %inlineActionMap = (
-             'b' => {
-                 'pre' => sub { return '<b>' },
-                 'mid' => \&_traverseInlineWithData,
-                 'post' => sub {return '</b>' },
-             },
-             'i' => {
-                 'pre' => sub { return '<i>' },
-                 'mid' => \&_traverseInlineWithData,
-                 'post' => sub { return '</i>' },
-             },
-             'tt' => {
-                 'pre' => sub { return '<tt>' },
-                 'mid' => \&_traverseInlineWithData,
-                 'post' => sub { return '</tt>' },
-             },
-             'text' => {
-                 'pre' => sub { return },
-                 'mid' => \&_printInlineData,
-                 'post' => sub { return }
-             },
-             'nowiki' => {
-                 'pre' => sub { return },
-                 'mid' => \&_printInlineData,
-                 'post' => sub { return }
-             },
-             'image' => {
-                 'pre' => sub { return },
-                 'mid' => sub { my $node = shift;
-                                return '<img src="' . $node->href .
-                                    '" />'; },
-                 'post' => sub { return }
-             },
-             );
+sub openTag {
+    my $node = shift;
+
+    return '<' . $node->type . ">\n";
+}
+
+sub closeTag {
+    my $node = shift;
+
+    return '</' . $node->type . '>';
+}
+
+sub closeTagWithNewline {
+    my $node = shift;
+
+    return &closeTag($node) . "\n";
+}
+
+sub openTagWithNid {
+    my $node = shift;
+
+    return &openTag($node) . &_anchor($node->id);
+}
+
+sub closeTagWithNid {
+    my $node = shift;
+
+    return &_nid($node->id) . &closeTag($node);
+}
+
+# inline node event handlers
+
+sub inlineContent {
+    my $node = shift;
+
+    return &_quoteHtml($node->content);
+}
+
+sub openLinkTag {
+    my $node = shift;
+    my $outputString;
+
+    $outputString = '<a href="' . $node->href . '">';
+    $outputString .= '[' if ($node->type eq 'link');
+    return $outputString;
+}
+
+sub closeLinkTag {
+    my $node = shift;
+    my $outputString;
+
+    $outputString = ']' if ($node->type eq 'link');
+    $outputString .= '</a>';
+    return $outputString;
+}
+
+sub wikiLink {
+    my $node = shift;
+    my $outputString;
+
+    my $pageName = $node->content;
+    $pageName =~ s/\#(\d+)$//;
+    my $pageNid = $1;
+
+    if ($node->content =~ /:/) {
+        $outputString .= '<a href="' .
+            &PurpleWiki::Page::getInterWikiLink($pageName);
+        $outputString .= "#nid$pageNid" if ($pageNid);
+        $outputString .= '">' . $node->content . '</a>';
+    }
+    elsif (&PurpleWiki::Page::exists($pageName)) {
+        if ($node->type eq 'freelink') {
+            $outputString .= '<a href="' .
+                &PurpleWiki::Page::getFreeLink($node->content) .
+                '">';
+        }
+        else {
+            $outputString .= '<a href="' . &PurpleWiki::Page::getWikiWordLink($pageName);
+            $outputString .= "#nid$pageNid" if ($pageNid);
+            $outputString .= '">';
+        }
+        $outputString .= $node->content . '</a>';
+    }
+    else {
+        if ($node->type eq 'freelink') {
+            $outputString .= '[' . $node->content . ']';
+            $outputString .= '<a href="' .
+                &PurpleWiki::Page::getFreeLink($node->content) .
+                '">';
+        }
+        else {
+            $outputString .= $node->content;
+            $outputString .= '<a href="' . &PurpleWiki::Page::getWikiWordLink($pageName) .
+                '">';
+        }
+        $outputString .= '?</a>';
+    }
+    return $outputString;
+}
+
+# functions
+
+sub registerHandlers {
+    $PurpleWiki::View::EventHandler::structuralHandler{section}->{pre} =
+        sub { push @sectionState, 'section'; return ''; };
+    $PurpleWiki::View::EventHandler::structuralHandler{section}->{post} =
+        sub { pop @sectionState; return ''; };
+
+    $PurpleWiki::View::EventHandler::structuralHandler{indent}->{pre} =
+        sub { return "<div class=\"indent\">\n"; };
+    $PurpleWiki::View::EventHandler::structuralHandler{indent}->{post} = \&closeTagWithNewline;
+
+    $PurpleWiki::View::EventHandler::structuralHandler{ul}->{pre} = \&openTag;
+    $PurpleWiki::View::EventHandler::structuralHandler{ul}->{post} = \&closeTagWithNewline;
+
+    $PurpleWiki::View::EventHandler::structuralHandler{ol}->{pre} = \&openTag;
+    $PurpleWiki::View::EventHandler::structuralHandler{ol}->{post} = \&closeTagWithNewline;
+
+    $PurpleWiki::View::EventHandler::structuralHandler{dl}->{pre} = \&openTag;
+    $PurpleWiki::View::EventHandler::structuralHandler{dl}->{post} = \&closeTagWithNewline;
+
+    $PurpleWiki::View::EventHandler::structuralHandler{h}->{pre} =
+        sub { my $node = shift;
+              return '<h' . &_headerLevel . '>' . &_anchor($node->id); };
+    $PurpleWiki::View::EventHandler::structuralHandler{h}->{post} =
+        sub { my $node = shift;
+              return &_nid($node->id) . '</h' . &_headerLevel . '>'; };
+
+    $PurpleWiki::View::EventHandler::structuralHandler{p}->{pre} = \&openTagWithNid;
+    $PurpleWiki::View::EventHandler::structuralHandler{p}->{post} = \&closeTagWithNid;
+
+    $PurpleWiki::View::EventHandler::structuralHandler{li}->{pre} = \&openTagWithNid;
+    $PurpleWiki::View::EventHandler::structuralHandler{li}->{post} = \&closeTagWithNid;
+
+    $PurpleWiki::View::EventHandler::structuralHandler{dd}->{pre} = \&openTagWithNid;
+    $PurpleWiki::View::EventHandler::structuralHandler{dd}->{post} = \&closeTagWithNid;
+
+    $PurpleWiki::View::EventHandler::structuralHandler{dt}->{pre} = \&openTagWithNid;
+    $PurpleWiki::View::EventHandler::structuralHandler{dt}->{post} = \&closeTagWithNid;
+
+    $PurpleWiki::View::EventHandler::structuralHandler{pre}->{pre} = \&openTagWithNid;
+    $PurpleWiki::View::EventHandler::structuralHandler{pre}->{post} = \&closeTagWithNid;
+
+    $PurpleWiki::View::EventHandler::inlineHandler{b}->{pre} = \&openTag;
+    $PurpleWiki::View::EventHandler::inlineHandler{b}->{post} = \&closeTag;
+
+    $PurpleWiki::View::EventHandler::inlineHandler{i}->{pre} = \&openTag;
+    $PurpleWiki::View::EventHandler::inlineHandler{i}->{post} = \&closeTag;
+
+    $PurpleWiki::View::EventHandler::inlineHandler{tt}->{pre} = \&openTag;
+    $PurpleWiki::View::EventHandler::inlineHandler{tt}->{post} = \&closeTag;
+
+    $PurpleWiki::View::EventHandler::inlineHandler{text}->{main} = \&inlineContent;
+
+    $PurpleWiki::View::EventHandler::inlineHandler{nowiki}->{main} = \&inlineContent;
+
+    $PurpleWiki::View::EventHandler::inlineHandler{image}->{main} =
+        sub { my $node = shift;
+              return '<img src="' . $node->href . '" />'; };
+
+    $PurpleWiki::View::EventHandler::inlineHandler{link}->{pre} = \&openLinkTag;
+    $PurpleWiki::View::EventHandler::inlineHandler{link}->{main} = \&inlineContent;
+    $PurpleWiki::View::EventHandler::inlineHandler{link}->{post} = \&closeLinkTag;
+
+    $PurpleWiki::View::EventHandler::inlineHandler{url}->{pre} = \&openLinkTag;
+    $PurpleWiki::View::EventHandler::inlineHandler{url}->{main} = \&inlineContent;
+    $PurpleWiki::View::EventHandler::inlineHandler{url}->{post} = \&closeLinkTag;
+
+    $PurpleWiki::View::EventHandler::inlineHandler{wikiword}->{main} = \&wikiLink;
+    $PurpleWiki::View::EventHandler::inlineHandler{freelink}->{main} = \&wikiLink;
+}
 
 sub view {
     my ($wikiTree, %params) = @_;
 
-    return &_traverseStructural($wikiTree->root->children, 0);
+    &registerHandlers;
+    return &PurpleWiki::View::EventHandler::view($wikiTree, %params);
 }
 
-sub _traverseStructural {
-    my ($nodeListRef, $indentLevel) = @_;
-    my $outputString;
-
-    if ($nodeListRef) {
-        foreach my $node (@{$nodeListRef}) {
-            if (defined($structuralActionMap{$node->type})) {
-                $outputString .=
-                    &{$structuralActionMap{$node->type}{'pre'}}($node->id);
-                $outputString .=
-                    &{$structuralActionMap{$node->type}{'mid'}}($node,
-                                                                $indentLevel);
-                $outputString .=
-                    &{$structuralActionMap{$node->type}{'post'}}($node->id);
-            } 
-            $outputString .= &_terminateLine unless ($node->type eq 'section');
-        }
-    }
-    return $outputString;
-}
-
-sub _terminateLine {
-    return "\n";
-}
-
-sub _traverseInlineIfContent {
-    my $structuralNode = shift;
-    my $indentLevel = shift;
-    if ($structuralNode->content) {
-        return _traverseInline($structuralNode->content, $indentLevel);
-    }
-}
-
-sub _traverseInlineWithData {
-    my $inlineNode = shift;
-    my $indentLevel = shift;
-    return _traverseInline($inlineNode->children, $indentLevel);
-}
-
-sub _printInlineData {
-    my $inlineNode = shift;
-    return &_quoteHtml($inlineNode->content);
-}
-
-sub _traverseStructuralWithChild {
-    my $structuralNode = shift;
-    my $indentLevel = shift;
-    return _traverseStructural($structuralNode->children, $indentLevel + 1);
-}
-
-sub _traverseInline {
-    my ($nodeListRef, $indentLevel) = @_;
-    my $outputString;
-
-    foreach my $inlineNode (@{$nodeListRef}) {
-        if ($inlineNode->type eq 'link' || $inlineNode->type eq 'url') {
-            $outputString .= '<a href="' . $inlineNode->href . '">';
-            $outputString .= '[' if ($inlineNode->type eq 'link');
-            $outputString .= &_quoteHtml($inlineNode->content);
-            $outputString .= ']' if ($inlineNode->type eq 'link');
-            $outputString .= '</a>';
-        }
-        elsif ($inlineNode->type eq 'wikiword' || $inlineNode->type eq 'freelink') {
-            my $pageName = $inlineNode->content;
-            $pageName =~ s/\#(\d+)$//;
-            my $pageNid = $1;
-            if ($inlineNode->content =~ /:/) {
-                $outputString .= '<a href="' . &PurpleWiki::Page::getInterWikiLink($pageName);
-
-                $outputString .= "#nid$pageNid" if ($pageNid);
-                $outputString .= '">' . $inlineNode->content . '</a>';
-            }
-            elsif (&PurpleWiki::Page::exists($pageName)) {
-                if ($inlineNode->type eq 'freelink') {
-                    $outputString .= '<a href="' . &PurpleWiki::Page::getFreeLink($inlineNode->content) .
-                        '">';
-                }
-                else {
-                    $outputString .= '<a href="' . &PurpleWiki::Page::getWikiWordLink($pageName);
-                    $outputString .= "#nid$pageNid" if ($pageNid);
-                    $outputString .= '">';
-                }
-                $outputString .= $inlineNode->content . '</a>';
-            }
-            else {
-                if ($inlineNode->type eq 'freelink') {
-                    $outputString .= '[' . $inlineNode->content . ']';
-                    $outputString .= '<a href="' . &PurpleWiki::Page::getFreeLink($inlineNode->content) .
-                        '">';
-                }
-                else {
-                    $outputString .= $inlineNode->content;
-                    $outputString .= '<a href="' . &PurpleWiki::Page::getWikiWordLink($pageName) .
-                        '">';
-                }
-                $outputString .= '?</a>';
-            }
-        }
-        elsif (defined($inlineActionMap{$inlineNode->type})) {
-            $outputString .= &{$inlineActionMap{$inlineNode->type}{'pre'}};
-            $outputString .= &{$inlineActionMap{$inlineNode->type}{'mid'}}($inlineNode,
-                                                          $indentLevel);
-            $outputString .= &{$inlineActionMap{$inlineNode->type}{'post'}};
-        }
-    }
-    return $outputString;
-}
+# private
 
 sub _quoteHtml {
     my ($html) = @_;
@@ -285,13 +240,13 @@ sub _headerLevel {
     return $headerLevel;
 }
 
-sub _printAnchor {
+sub _anchor {
     my $nid = shift;
 
     return '<a name="nid0' . $nid . '" id="nid0' . $nid . '"></a>' if ($nid);
 }
 
-sub _printNid {
+sub _nid {
     my $nid = shift;
 
     if ($nid) {
