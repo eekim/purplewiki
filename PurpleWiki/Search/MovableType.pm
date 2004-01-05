d3d292e80be9333a2678e48cf7c6b1c590c4c382
cdent <cdent@8e67a9af-bad3-0310-9569-fa3748d2677d>
2004-01-05 22:46:21 +0000

    Adding copyright info and pod framework. Mostly as a test of the syncmail diffing.

--- PurpleWiki/Search/MovableType.pm
+++ PurpleWiki/Search/MovableType.pm
@@ -1,8 +1,32 @@
 # PurpleWiki::Search::MovableType.pm
 # vi:ai:sm:et:sw=4:ts=4
 #
-# $Id: MovableType.pm,v 1.2 2004/01/01 01:20:35 cdent Exp $
+# $Id: MovableType.pm,v 1.3 2004/01/05 22:46:21 cdent Exp $
 #
+# Copyright (c) Blue Oxen Associates 2002-2004.  All rights reserved.
+#
+# This file is part of PurpleWiki.  PurpleWiki is derived from:
+#
+#   UseModWiki v0.92          (c) Clifford A. Adams 2000-2001
+#   AtisWiki v0.3             (c) Markus Denker 1998
+#   CVWiki CVS-patches        (c) Peter Merel 1997
+#   The Original WikiWikiWeb  (c) Ward Cunningham
+#
+# PurpleWiki is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
 
 package PurpleWiki::Search::MovableType;
 
@@ -87,3 +111,32 @@ sub _initMT() {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+PurpleWiki::Search::MovableType - Search MovableType blogs
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+
+
+=head1 METHODS
+
+
+
+=head1 AUTHOR
+
+Chris Dent, E<lt>cdent@blueoxen.orgE<gt>
+
+=head1 SEE ALSO
+
+L<PurpleWiki::Search::Engine>.
+L<PurpleWiki::Search::Result>.
+
+=cut
+
