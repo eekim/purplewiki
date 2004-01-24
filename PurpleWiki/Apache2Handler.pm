21bc13b3b5efd3f9296db37a3f8016c2480fcebd
cdent <cdent@8e67a9af-bad3-0310-9569-fa3748d2677d>
2004-01-24 02:48:00 +0000

    Adding Apache2Hanlder as a mod_perl 2 handler for *.wiki files.
    Needs documentation and a sort of howto/use case thingie.

---
+++ PurpleWiki/Apache2Handler.pm
@@ -0,0 +1,87 @@
+# PurpleWiki::Apache2Handler.pm
+# vi:ai:sw=4:ts=4:et:sm
+#
+# $Id: Apache2Handler.pm,v 1.1 2004/01/24 02:48:00 cdent Exp $
+#
+# Copyright (c) Blue Oxen Associates 2002-2003.  All rights reserved.
+#
+# This file is part of PurpleWiki.  PurpleWiki is derived from:
+#
+#   UseModWiki v0.92          (c) Clifford A. Adams 2000-2001
+#   AtisWiki v0.3             (c) Markus Denker 1998
+#   CVWiki CVS-patches        (c) Peter Merel 1997
+#   The Original WikiWikiWeb  (c) Ward Cunningham
+#
+# PurpleWiki is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+package PurpleWiki::Apache2Handler;
+
+use strict;
+use lib '/home/cdent/src/PurpleWiki';
+use IO::File;
+use PurpleWiki::Config;
+use PurpleWiki::Parser::WikiText;
+use Apache::RequestRec ();
+use Apache::RequestIO ();
+use Apache::URI;
+use Apache::Const -compile => qw(OK);
+
+my $CONFIG = '/home/cdent/testpurple';
+my $CSS = '/~cdent/purple.css';
+
+sub handler {
+    my $r = shift;
+
+    $r->content_type('text/html');
+
+    my $file = $r->filename();
+    my $url = $r->construct_url();
+
+    my $content = readFile($file);
+    my $purpleConfig = new PurpleWiki::Config($CONFIG);
+    my $wikiParser = new PurpleWiki::Parser::WikiText();
+    my $wiki = $wikiParser->parse($content, 
+        config => $purpleConfig,
+        wikiword => 1,
+        css_file => $CSS,
+        url => $url,
+    );
+
+    print $wiki->view('xhtml', 
+        config => $purpleConfig,
+        wikiword => 1,
+        css_file => $CSS,
+        url => $url,
+    );
+
+    # FIXME: sometimes okay is not the desired return code
+    return Apache::OK;
+
+}
+
+sub readFile {
+    my $file = shift;
+    my $fileContent = '';
+
+    my $fh = new IO::File();
+    $fh->open($file) || die "unable to open $file: $!";
+    return join('', $fh->getlines);
+}
+
+
+
+1;
