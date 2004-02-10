58924742cf92d5c05eb4ba23c524310656b9195d
eekim <eekim@8e67a9af-bad3-0310-9569-fa3748d2677d>
2004-02-10 23:55:18 +0000

    Integrated Jon's very minor change to surround NIDs with parentheses.
      http://purplewiki.blueoxen.net/forums/devel/2004-01/msg00046.html
    
    I think this is better than just the number because it differentiates the
    NID from HIDs.  (The new NID addressing format looks very similar to HIDs.)
    It's also the convention I used in the original Purple system.

--- PurpleWiki/View/wikihtml.pm
+++ PurpleWiki/View/wikihtml.pm
@@ -1,7 +1,7 @@
 # PurpleWiki::View::wikihtml.pm
 # vi:ai:sm:ts=4:sw=4:et
 #
-# $Id: wikihtml.pm,v 1.11 2004/01/21 23:24:08 cdent Exp $
+# $Id: wikihtml.pm,v 1.12 2004/02/10 23:55:18 eekim Exp $
 #
 # Copyright (c) Blue Oxen Associates 2002-2003.  All rights reserved.
 #
@@ -300,7 +300,7 @@ sub _nid {
     my $nidFace = '#';
 
     if ($params{config}->ShowNid) {
-        $nidFace = $nid;
+        $nidFace = "($nid)";
     }
 
     if ($nid) {
