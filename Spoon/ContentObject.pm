bd74166a6a37512a90da0f69006411099161e13a
gerry <gerry@8e67a9af-bad3-0310-9569-fa3748d2677d>
2004-08-25 16:51:21 +0000

    Added the modules from Kwiki that are likely to be used

---
+++ Spoon/ContentObject.pm
@@ -0,0 +1,31 @@
+package Kwiki::ContentObject;
+use strict;
+use warnings;
+use Spoon::ContentObject '-Base';
+
+1;
+
+__DATA__
+
+=head1 NAME 
+
+Kwiki::ContentObject - Kwiki Content Object Base Class
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Brian Ingerson <INGY@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004. Brian Ingerson. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=cut
---
+++ Spoon/DataObject.pm
@@ -0,0 +1,40 @@
+package Spoon::DataObject;
+use strict;
+use warnings;
+use Spoon::Base '-base';
+
+stub 'class_id';
+field 'id';
+
+sub name {
+    my $self->{name} = shift if @_;
+    return $self->{name} if defined $self->name;
+    $self->{name} = $self->uri_unescape($self->id);
+}
+
+1;
+
+__DATA__
+
+=head1 NAME
+
+Spoon::DataObject - Spoon Data Object Base Class
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Brian Ingerson <INGY@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004. Brian Ingerson. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=cut
