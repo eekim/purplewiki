8fb09d14e3370f6895770a8161c0017f6b72df02
eekim <eekim@8e67a9af-bad3-0310-9569-fa3748d2677d>
2004-11-23 18:14:29 +0000

    Added some skeleton docs for PurpleWiki::Archive::Sequence.
    Refactored the command-line options for backendConvert.pl, replacing
    the homegrown parsing with Getopt::Std.  Started initial work of 
    integrating sequenceIndex.pl functionality into backendConvert.pl.
    One big change remains.  sequenceIndex.pl rebuilds the index with no
    thought of the other apps that use the sequence database.  It needs
    to rebuild only the Wiki parts of the index, retaining the rest of the
    index as well as the last used NID.

--- PurpleWiki/Archive/Sequence.pm
+++ PurpleWiki/Archive/Sequence.pm
@@ -1,4 +1,36 @@
+# PurpleWiki::Archive::Sequence
+# vi:sw=4:ts=4:ai:sm:et:tw=0
 #
+# $Id$
+#
+# Copyright (c) Blue Oxen Associates 2002-2003.  All rights reserved.
+#
+# This file is part of PurpleWiki.  PurpleWiki is derived from:
+#
+#   UseModWiki v0.92          (c) Clifford A. Adams 2000-2001
+#   AtisWiki v0.3             (c) Markus Denker 1998
+#   CVWiki CVS-patches        (c) Peter Merel 1997
+#   The Original WikiWikiWeb  (c) Ward Cunningham
+#
+# PurpleWiki is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+our $VERSION;
+$VERSION = sprintf("%d", q$Id$ =~ /\s(\d+)\s/);
+
 use PurpleWiki::View::Filter;
 package PurpleWiki::Archive::Sequence;
 
@@ -55,3 +87,36 @@ sub _getSequencer {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+PurpleWiki::Archive::Sequence - 
+
+=head1 DESCRIPTION
+
+
+
+=head1 METHODS
+
+=head2 getCurrentValue($pages)
+
+
+
+=head2 setCurrentValue($pages, $value)
+
+
+
+=head2 updateNIDs($pages, $url, $tree, $maxRef)
+
+
+
+=head1 AUTHOR
+
+Gerry Gleason, E<lt>gerry@geraldgleason.comE<gt>
+
+=head1 SEE ALSO
+
+L<PurpleWiki::Sequence>.
+
+=cut
