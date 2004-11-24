a9dde1fe221bfcffb7f93404e7281a5001b55eca
gerry <gerry@8e67a9af-bad3-0310-9569-fa3748d2677d>
2004-11-24 02:54:01 +0000

    Update Manifest and remove some extra config files

--- t/runTest.pl
+++ t/runTest.pl
@@ -1,3 +1,40 @@
+#
+# runTest.pl - PurpleWiki
+#
+# $Id: runTest.pl 567 2004-11-17 17:13:33Z gerry $
+#
+# Copyright (c) Blue Oxen Associates 2002.  All rights reserved.
+#
+# This file is part of PurpleWiki.  PurpleWiki is derived from:
+#
+#   UseModWiki v0.92          (c) Clifford A. Adams 2000-2001
+#   AtisWiki v0.3             (c) Markus Denker 1998
+#   CVWiki CVS-patches        (c) Peter Merel 1997
+#   The Original WikiWikiWeb  (c) Ward Cunningham
+#
+# PurpleWiki is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+#    Free Software Foundation, Inc.
+#    59 Temple Place, Suite 330
+#    Boston, MA 02111-1307 USA
+
+#
+# runTest -- used by r/runlogm.pl and t/runwik*.t tests
+#
+# This version of the test harness runs each request sequentially in a single
+# process.  The r/runlog.pl version forks a process for each test and does not
+# use this function which does it in-process.
+#
 sub runTest {
 my ($q, $out) = @_;
     if (!open(STDOUT, ">$out")) {
